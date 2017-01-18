3c2a15d57d1da22a9386c62a94ecf78051a31c69
int3ro <int3ro@gmail.com>
2017-01-19 00:49:00 +0100

    Changed colors in DPS mode. Bugfix.

--- NameplatesThreat.lua
+++ NameplatesThreat.lua
@@ -7,37 +7,40 @@ local isOfftank = function(target)
     return false
 end
 hooksecurefunc("CompactUnitFrame_OnUpdate", function(frame)
-	if C_NamePlate.GetNamePlateForUnit(frame.unit) ~= C_NamePlate.GetNamePlateForUnit("player") and not UnitIsPlayer(frame.unit) and not CompactUnitFrame_IsTapDenied(frame) then
-		local threat = UnitThreatSituation("player", frame.unit) or 0
+	if C_NamePlate.GetNamePlateForUnit(frame.unit) ~= C_NamePlate.GetNamePlateForUnit("player") 
+		--and UnitIsEnemy("player", frame.unit) 
+		and not UnitIsPlayer(frame.unit) 
+		and not CompactUnitFrame_IsTapDenied(frame) then
+		--[[
+			threat:
+			-1 = not on threat table.
+			0 = not tanking, lower threat than tank.
+			1 = not tanking, higher threat than tank.
+			2 = insecurely tanking, another unit have higher threat but not tanking.
+			3 = securely tanking, highest threat
+		]]--
+		local threat = UnitThreatSituation("player", frame.unit) or -1
 		if GetSpecializationRole(GetSpecialization()) == "TANK" then
-			if threat == 3 then
-				-- Securely tanking	
-				r, g, b = 0, 0.5, 0
-			elseif threat == 2 then
-				-- Tanking, but somebody else has higher threat (losing threat)
-				r, g, b = 1, 0.5, 0		
-			elseif threat == 1 then
-				-- Not tanking, but higher threat than tank
-				r, g, b = 1, 1, 0.4
+			if threat == 3 then				
+				r, g, b = 0.0, 0.5, 0.0
+			elseif threat == 2 then				
+				r, g, b = 1.0, 0.5, 0.0
+			elseif threat == 1 then				
+				r, g, b = 1.0, 1.0, 0.4
+			elseif isOfftank(frame.unit.."target") then
+				r, g, b = 0.2, 0.5, 0.9
 			else
-				-- Not tanking
-				local target = frame.unit.."target"
-				if isOfftank(target) then
-					r, g, b = 0.2, 0.5, 0.9
-				else
-					r, g, b = 1, 0, 0
-				end
+				r, g, b = 1.0, 0.0, 0.0
 			end
 		else
 			if threat >= 2 then
-				-- tanking
-				r, g, b = 1, 0.5, 0	
-			elseif threat == 1 then
-				-- Not tanking, but higher threat than tank				
-				r, g, b = 1, 1, 0.4
+				r, g, b = 1.0, 0.0, 0.0
+			elseif threat == 1 then 							
+				r, g, b = 1.0, 1.0, 0.4	
+			elseif threat == 0 then
+				r, g, b = 0.0, 0.5, 0.0
 			else
-				-- Not tanking
-				r, g, b = 1, 0, 0	
+				r, g, b = 1.0, 0.0, 0.0				
 			end
 		end
 		frame.healthBar:SetStatusBarColor(r, g, b, 1)
