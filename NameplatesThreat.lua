4f6cc2e9b728a69abe38123a45a175a2145abf4d
eXochron <exochron@gamerstate.de>
2017-02-22 18:16:29 +0100

    added timer to cleanup some broken nameplates

--- NameplatesThreat.lua
+++ NameplatesThreat.lua
@@ -28,7 +28,7 @@ local function updateHealthColor(frame)
         end
     end
 end
--- called constantly in combat. so we only reset the color after it was actually changed.
+-- This function is called constantly during combat. The color is only going to be reset after it was actually changed.
 hooksecurefunc("CompactUnitFrame_UpdateHealthColor", updateHealthColor)
 
 local function collectOffTanks()
@@ -63,7 +63,8 @@ local function isOfftankTanking(mobUnit)
 end
 
 local function updateThreatColor(frame)
-    if UnitIsEnemy("player", frame.unit) and not CompactUnitFrame_IsTapDenied(frame) then
+    local unit = frame.unit
+    if UnitIsEnemy("player", unit) and not CompactUnitFrame_IsTapDenied(frame) then
         --[[
             threat:
             -1 = not on threat table.
@@ -71,10 +72,10 @@ local function updateThreatColor(frame)
             1 = not tanking, higher threat than tank.
             2 = insecurely tanking, another unit have higher threat but not tanking.
             3 = securely tanking, highest threat
-            9 = offtank is tanking, our created state
+            9 = offtank is tanking
         ]] --
-        local threat = UnitThreatSituation("player", frame.unit) or -1
-        if playerRole == "TANK" and threat < 1 and isOfftankTanking(frame.unit) then
+        local threat = UnitThreatSituation("player", unit) or -1
+        if playerRole == "TANK" and threat < 1 and isOfftankTanking(unit) then
             threat = 9
         end
 
@@ -120,28 +121,43 @@ local function updateThreatColor(frame)
     end
 end
 
+local function updateAllNameplates()
+    for _, nameplate in pairs(C_NamePlate.GetNamePlates()) do
+        updateThreatColor(nameplate.UnitFrame)
+    end
+end
+
+local polishTimer
 local myFrame = CreateFrame("frame")
-myFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED");
-myFrame:RegisterEvent("NAME_PLATE_UNIT_REMOVED");
-myFrame:RegisterEvent("NAME_PLATE_UNIT_ADDED");
 myFrame:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE");
+myFrame:RegisterEvent("NAME_PLATE_UNIT_ADDED");
+myFrame:RegisterEvent("NAME_PLATE_UNIT_REMOVED");
 myFrame:RegisterEvent("PLAYER_ROLES_ASSIGNED");
-myFrame:RegisterEvent("GROUP_ROSTER_UPDATE");
+myFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED");
 myFrame:SetScript("OnEvent", function(self, event, arg1)
     if event == "UNIT_THREAT_SITUATION_UPDATE" then
         if not playerRole then
             updatePlayerRole()
         end
-        for _, nameplate in pairs(C_NamePlate.GetNamePlates()) do
-            updateThreatColor(nameplate.UnitFrame)
+        updateAllNameplates()
+
+        -- Sometimes single nameplates got the wrong Threatsituation and color.
+        -- It keeps that color until this event is called again, which can take a while.
+        -- The polishTimer starts after a second without situation update.
+        if polishTimer then
+            polishTimer:Cancel()
         end
+        polishTimer = C_Timer.NewTimer(1, function()
+            updateAllNameplates()
+            polishTimer = nil
+        end)
     elseif event == "NAME_PLATE_UNIT_ADDED" then
         local nameplate = C_NamePlate.GetNamePlateForUnit(arg1)
         updateThreatColor(nameplate.UnitFrame)
     elseif event == "NAME_PLATE_UNIT_REMOVED" then
         local nameplate = C_NamePlate.GetNamePlateForUnit(arg1)
         resetFrame(nameplate.UnitFrame)
-    elseif event == "PLAYER_ROLES_ASSIGNED" or event == "GROUP_ROSTER_UPDATE" then
+    elseif event == "PLAYER_ROLES_ASSIGNED" then
         offTanks = collectOffTanks()
     elseif event == "PLAYER_SPECIALIZATION_CHANGED" then
         updatePlayerRole()
